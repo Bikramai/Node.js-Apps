17b2f85bba113e70a4f65c0f28e0afb6ed06a189
bikramia <bikram.rhce@gmail.com>
2024-09-01 14:38:29 -0400

    Global Object

---
+++ Section2-Node-Module-System/globalobject.js
@@ -0,0 +1,17 @@
+// Some of global objects in javascript.
+// console.log(); // console object what we call global object.
+
+// setTimeout();
+// clearTimeout();
+
+// setInterval()
+// clearInterval()
+
+// // window.console.log
+
+// All the above object are belong to window object.
+
+// window.setTimeout
+
+var message = '';
+console.log(global.message)
