af0e2d1db8a9f5ce6c40de278918d7ce2ac5756b
bikramia <bikram.rhce@gmail.com>
2024-09-09 19:15:22 -0400

    routeparameters.js

--- Section4-Building-RESTful-APIs-Using-Express/index.js
+++ Section4-Building-RESTful-APIs-Using-Express/index.js
@@ -9,9 +9,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 })
 
-app.listen(3000, () => console.log('Listening on port 3000...'))
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
 
-// app.post()
-// app.put()
-// app.delete()
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port port ${port}...`))
