b617a01d018c9d804a63046060d7f75a2b8175ed
bikramia <bikram.rhce@gmail.com>
2024-09-24 14:53:49 -0400

    Creating Custom Middleware function - logger, auth

---
+++ Section5-Express-Advanced-Topics/auth.js
@@ -0,0 +1,6 @@
+function auth(req, res, nest) {
+    console.log('Authenticating....');
+    next();
+}
+
+module.exports = auth;
---
+++ Section5-Express-Advanced-Topics/logger.js
@@ -0,0 +1,6 @@
+function log(req, res, nest) {
+    console.log('Logging....');
+    next();
+}
+
+module.exports = log;
