3daee92b5f3a20bc5eb40f47df6df8a665e9b0b5
bikramia <bikram.rhce@gmail.com>
2024-10-25 07:16:42 -0400

    Templating Engines -using pug is not working need to fix

--- Section5-Express-Advanced-Topics/index.js
+++ Section5-Express-Advanced-Topics/index.js
@@ -1,114 +1,116 @@
-const Debug = require('debug')('app:startup');
-const config = require('config')
-const morgan = require('morgan');
-const helmet = require('helmet');
-const Joi = require('joi');
-const logger = require('./logger');
-const auth = require('./auth')
-const express = require('express');
+const Debug = require("debug")("app:startup");
+const config = require("config");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const Joi = require("joi");
+const logger = require("./logger");
+const auth = require("./auth");
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views'); // default value and optional if you want to overwrite the views
+app.set("view engine", "pug");
+app.set("views", "./views"); // default value and optional if you want to overwrite the views
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(helmet());
 
-if (app.get('env') === 'development') {
-    app.use(morgan('tiny'));
-    Debug('Morgan enabled...'); // console.log()
-    
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  Debug("Morgan enabled..."); // console.log()
 }
 
 // Configuration
-// console.log('Application Name: ' + config.get('name'));
-// console.log('Mail Server: ' + config.get('mail.host'));
-// console.log('Mail Password: ' + config.get('mail.password'));
+console.log("Application Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail Password: " + config.get("mail.password"));
 
 app.use(logger);
 
-app.use(auth); 
+app.use(auth);
 
 const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' },
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
-app.get('/', (req, res) =>{ 
-    res.render('index', { title: 'My Express App', message: 'Hello' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "My Express App", message: "Hello" });
 });
 
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-})
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found.');
-    res.send(course);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.post('/api/courses', (req, res) =>{
-    const { error } = validateCourse(req.body);  // error.result
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(error.details[0].message)
-        return; 
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
-    req.send(course);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
+  res.send(course);
 });
 
-app.put('/api/courses/:id', (req, res) =>{
-    // Look up the course
-    // If not existing, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found.');
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body); // error.result
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
 
-    const { error } = validateCourse(req.body);  // error.result
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(error.details[0].message)
-        return; 
-    }
-
-    // Update course
-    course.name = req.body.name;
-    // Return the upsated course
-    res.send(course);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  req.send(course);
 });
 
-app.delete('/api/courses/:id', (req, res) => {
-    // Look up the course
-    // Not existing, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) res.status(404).send('The course with the given ID was not found.');
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
 
-    // Delete
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+  const { error } = validateCourse(req.body); // error.result
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
 
-    // Return the same course
-    res.send(course);
-})
+  // Update course
+  course.name = req.body.name;
+  // Return the upsated course
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // Not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found.");
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  // Return the same course
+  res.send(course);
+});
 
 function validateCourse(course) {
-     // Validate
-    // If invalid, return 400 - Bad request
-    const schema = {
-        name: Joi.string().min(6).required()
-    };
+  // Validate
+  // If invalid, return 400 - Bad request
+  const schema = {
+    name: Joi.string().min(6).required(),
+  };
 
-    return Joi.validate(course, schema);
+  return Joi.validate(course, schema);
 }
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port port ${port}...`))
+app.listen(port, () => console.log(`Listening on port port ${port}...`));
