7d75df747e7f9ec63e03b913c133d361b03251d8
bikramia <bikram.rhce@gmail.com>
2024-10-28 07:24:54 -0400

    finally forgot to call the routes in express

--- Project-Vidly-App/index.js
+++ Project-Vidly-App/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.ude("./api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
