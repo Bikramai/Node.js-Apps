ef5b131643a54df94dd93eea5538bc17e1190fbf
bikramia <bikram.rhce@gmail.com>
2024-11-13 07:57:30 -0500

    Replacing Callbacks with Promises

--- Section6-Asynchronous-vs-Asynchronus-Code/async-demo/index.js
+++ Section6-Asynchronous-vs-Asynchronus-Code/async-demo/index.js
@@ -1,23 +1,17 @@
-console.log("before");
-getUser(1, getRepositories);
+console.log("Before");
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
+  });
+});
 console.log("After");
-
-function getRepositories(user) {
-  getRepositories(user.gitHubUsername, getCommits);
-}
-
-function getCommits(repos) {
-  getCommits(repo, displayCommits);
-}
-
-function displayCommits(commits) {
-  console.log(commits);
-}
 
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database...");
-    callback({ id: id, gitHubUsername: "bikram" });
+    callback({ id: id, gitHubUsername: "mosh" });
   }, 2000);
 }
 
@@ -25,5 +19,12 @@ function getRepositories(username, callback) {
   setTimeout(() => {
     console.log("Calling GitHub API...");
     callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["commit"]);
   }, 2000);
 }
