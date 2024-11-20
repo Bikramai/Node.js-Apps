46408ec890b4142d2b510dde36f456608fc3960b
bikramia <bikram.rhce@gmail.com>
2024-11-20 07:50:54 -0500

    Running Promises in Parallel -As soon as one/first async is complete

--- Section6-Asynchronous-vs-Asynchronus-Code/async-demo/parallel-promises.js
+++ Section6-Asynchronous-vs-Asynchronus-Code/async-demo/parallel-promises.js
@@ -1,7 +1,7 @@
-const p1 = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve) => {
   setTimeout(() => {
     console.log("Async operation 1...");
-    reject(new Error("because something failed."));
+    resolve(1);
   }, 2000);
 });
 const p2 = new Promise((resolve) => {
