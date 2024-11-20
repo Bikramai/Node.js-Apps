98da8ba4aeb1f8f5e5cb6e9d8e73f01e6b8dc887
bikramia <bikram.rhce@gmail.com>
2024-11-20 07:46:52 -0500

    Running Promises in Parallel -As soon as one/first async is complete

--- Section6-Asynchronous-vs-Asynchronus-Code/async-demo/parallel-promises.js
+++ Section6-Asynchronous-vs-Asynchronus-Code/async-demo/parallel-promises.js
@@ -11,6 +11,6 @@ const p2 = new Promise((resolve) => {
   }, 2000);
 });
 
-Promise.all([p1, p2])
+Promise.race([p1, p2])
   .then((result) => console.log(result))
   .catch((err) => console.log("Error", err.message));
