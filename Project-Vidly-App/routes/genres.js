f64d119518d8b7e432350f25fb5c39f75c0333de
bikramia <bikram.rhce@gmail.com>
2025-01-05 14:17:13 -0500

    Add Persistence to Genres API - GET single genre and but Error Postman is not connecting

--- Project-Vidly-App/routes/genres.js
+++ Project-Vidly-App/routes/genres.js
@@ -46,10 +46,11 @@ router.delete("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.get("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  
+  if (!genre)return res.status(404).send("The genre with the given ID was not found.");
+
   res.send(genre);
 });
 
